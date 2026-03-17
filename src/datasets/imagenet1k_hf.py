d1240f3fec0bfe72df8d05f6a38c77f361ca9f87
Adam Nguyen <Adanato@vt.edu>
2026-03-17 14:00:06 -0400

    Switch ImageNet loading to HuggingFace Hub
    Replace filesystem-based imagenet1k loader with HF datasets wrapper
    that loads ILSVRC/imagenet-1k from the hub, removing the need for
    local dataset copies.

---
+++ src/datasets/imagenet1k_hf.py
@@ -0,0 +1,67 @@
+# HuggingFace datasets wrapper for ImageNet-1K
+# Drop-in replacement for imagenet1k.py that loads from HF hub
+
+import torch
+from datasets import load_dataset
+from logging import getLogger
+
+logger = getLogger()
+
+
+class HFImageNet(torch.utils.data.Dataset):
+    """Wraps HuggingFace ILSVRC/imagenet-1k as a map-style dataset."""
+
+    def __init__(self, split='train', transform=None):
+        self.transform = transform
+        self.dataset = load_dataset('ILSVRC/imagenet-1k', split=split)
+        logger.info(f'Loaded HF ImageNet split={split}, len={len(self.dataset)}')
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        sample = self.dataset[index]
+        img = sample['image'].convert('RGB')
+        label = sample['label']
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, label
+
+
+def make_imagenet1k(
+    transform,
+    batch_size,
+    collator=None,
+    pin_mem=True,
+    num_workers=8,
+    world_size=1,
+    rank=0,
+    training=True,
+    drop_last=True,
+    # ignored params kept for interface compat
+    root_path=None,
+    image_folder=None,
+    copy_data=False,
+    subset_file=None,
+):
+    split = 'train' if training else 'validation'
+    dataset = HFImageNet(split=split, transform=transform)
+    logger.info('ImageNet HF dataset created')
+
+    dist_sampler = torch.utils.data.distributed.DistributedSampler(
+        dataset=dataset,
+        num_replicas=world_size,
+        rank=rank)
+
+    data_loader = torch.utils.data.DataLoader(
+        dataset,
+        collate_fn=collator,
+        sampler=dist_sampler,
+        batch_size=batch_size,
+        drop_last=drop_last,
+        pin_memory=pin_mem,
+        num_workers=num_workers,
+        persistent_workers=False)
+
+    logger.info('ImageNet HF data loader created')
+    return dataset, data_loader, dist_sampler
